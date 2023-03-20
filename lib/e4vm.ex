84aa642def11a8ddc9b8847dd61ea3a278723df0
Alexey Bolshakov <ua3mqj@gmail.com>
2023-03-20 17:28:12 +0300

    refactory and comments

--- lib/e4vm.ex
+++ lib/e4vm.ex
@@ -7,7 +7,7 @@ defmodule E4vm do
             ds: Structure.Stack.new(), # Стек данных
             ip: 0,                     # Указатель инструкций
             wp: 0,                     # Указатель слова
-            core: %{}                  # хранилище примитивов
+            primitives: %{}            # хранилище примитивов
 
 
   alias Structure.Stack
@@ -15,54 +15,50 @@ defmodule E4vm do
   @next    0
   @do_list 1
   @exit    2
-  @hello   3
 
   def new() do
-    core = %{
+    primitives = %{
       @next    => {E4vm, :next},
       @do_list => {E4vm, :do_list},
-      @exit    => {E4vm, :exit},
-      @hello   => {E4vm, :hello}
+      @exit    => {E4vm, :exit}
     }
 
-    mem = %{
-      0 => @next,
-      1 => @do_list,
-      2 => @exit,
-      3 => @hello,
-      4 => @do_list,
-      5 => @hello,
-      6 => @exit,
-      7 => @do_list,
-      8 => 4,
-      9 => @exit
-    }
-    %E4vm{core: core, mem: mem}
+    %E4vm{primitives: primitives}
   end
 
+  # Останавливаемся, если адрес 0
   def next(%E4vm{ip: 0} = vm) do
     "ip:#{vm.ip} wp:#{vm.wp}" |> IO.inspect(label: ">>>>>>>>>>>> next ok")
 
     vm
   end
 
+  # Суть интерпретации заключается в переходе по адресу в памяти и в исполнении инструкции, которая там указана.
   def next(vm) do
     # vm |> IO.inspect(label: ">>>>>>>>>>>> next")
     "ip:#{vm.ip} wp:#{vm.wp}" |> IO.inspect(label: ">>>>>>>>>>>> next   ")
 
+    # выбираем адрес следующей инструкции
     next_wp = vm.mem[vm.ip]
+    # увеличиваем указатель инструкций
     next_ip = vm.ip + 1
     new_vm = %E4vm{vm | ip: next_ip, wp: next_wp}
 
-    {m, f} = new_vm.core[new_vm.mem[new_vm.wp]]
-    #  |> IO.inspect(label: ">>>>>>>>>>>> next execute")
+    # по адресу следующего указателя на слово
+    # выбираем адрес инструкции из памяти
+    # и по адресу определяем команду с помощью хранилища примитовов
+    {m, f} = new_vm.primitives[new_vm.mem[new_vm.wp]]
+      |> IO.inspect(label: ">>>>>>>>>>>> next execute")
 
+    # выполняем эту команду
     next_new_vm = apply(m, f, [new_vm])
 
     next(next_new_vm)
-    # next_new_vm
   end
 
+  # Каждое пользовательское слово начинается с команды DoList,
+  # задача которой — сохранить текущий адрес интерпретации на стеке
+  # и установить адрес интерпретации следующего слова.
   def do_list(vm) do
     "ip:#{vm.ip} wp:#{vm.wp}" |> IO.inspect(label: ">>>>>>>>>>>> do_list")
 
@@ -72,6 +68,8 @@ defmodule E4vm do
     %E4vm{vm | ip: next_ip, rs: next_rs}
   end
 
+  # команда для выхода из слова
+  # восстанавливает адрес указателя инструкций IP со стека возвратов RS
   def exit(vm) do
     "ip:#{vm.ip} wp:#{vm.wp}" |> IO.inspect(label: ">>>>>>>>>>>> exit   ")
 
@@ -79,12 +77,5 @@ defmodule E4vm do
     {:ok, next_rs} = Stack.pop(vm.rs)
 
     %E4vm{vm | ip: next_ip, rs: next_rs}
-  end
-
-  def hello(vm) do
-    "ip:#{vm.ip} wp:#{vm.wp}" |> IO.inspect(label: ">>>>>>>>>>>> hello  ")
-
-    IO.puts("Hello")
-    vm
   end
 end
--- test/e4vm_test.exs
+++ test/e4vm_test.exs
@@ -10,22 +10,36 @@ defmodule E4vmTest do
   test "greets the world" do
     Process.register(self(), :test_proc)
 
-    entry_point = 7
+    vm = E4vm.new
 
-    core = %{
-      @next    => {E4vm, :next},
-      @do_list => {E4vm, :do_list},
-      @exit    => {E4vm, :exit},
-      @hello   => {E4vmTest, :hello}
+    primitives = vm.primitives
+      |> Map.merge(%{@hello   => {E4vmTest, :hello}})
+
+    mem = %{
+      # указатели
+      0 => @next,     # 0 адрес 0 команда - next
+      1 => @do_list,  # 1 адрес 1 команда - do_list
+      2 => @exit,     # 2 адрес 2 команда - exit
+      3 => @hello,    # 3 вдрес 3 команда - hello
+      # -------------------------------------------
+      # program
+      4 => @do_list,  # 3) сохраняем адрес интерпретации IP = 9 на стеке возвратов, затем устанавливаем IP = WP + 1 = 5
+      5 => @hello,    # 4) выводим на экран сообщение
+      6 => @exit,     # 5) выходим из слова, восстанавливаем IP = 9 со стека возвратов
+      7 => @do_list,  # 1) точка входа в подпрограмму - выполнить команду по адресу 4
+      8 => 4,         # 2) вызов подпрограммы по адресу 4, устанавливаем WP = 4
+      9 => @exit
     }
 
-    vm = E4vm.new
-    vm2 = %E4vm{vm | ip: 0, wp: entry_point, core: core}
-    # |> IO.inspect(label: ">>>>>>>>>>>> befre do_list")
-    |> E4vm.do_list()
-    # |> IO.inspect(label: ">>>>>>>>>>>> befre next")
-    |> E4vm.next()
-    # |> IO.inspect(label: ">>>>>>>>>>>> after next")
+    ip = 0
+    entry_point = 7
+
+    # ставим начальный адрес IP=0 чтобы по завершении выполнения
+    # и возврату в 0 закончилась работа цикла next() - там выход на 0.
+    # ставим wp указатель на слово wp=7 - адрес входа в программу
+    %E4vm{vm | ip: ip, wp: entry_point, primitives: primitives, mem: mem}
+      |> E4vm.do_list() # выполняем команду начала интерпретации слова, сохраняя IP = 0 на стеке возвратов
+      |> E4vm.next()    # запускаем адресный интерпретатор
 
     assert_receive :hello
   end
